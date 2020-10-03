e48c2572177f1a08d6b49819ecb5225dbf8d10bb
AlfredoGuadalupe <54385258+AlfredoGuadalupe@users.noreply.github.com>
2020-10-02 21:02:23 -0500

    Update Reporte1.c

--- Reporte1.c
+++ Reporte1.c
@@ -3,5 +3,18 @@
 #include <stdio.h>
 
 int main(void){
-
+  short i,j,k;
+  int edo=3;
+  int mes=3;
+  int anio=4;
+  float temperatura[edo][mes][anio] = srand();
+  
+  for(i=0:i<=3;i++){
+    for(j=0:j<=3;j++){
+      for(k=0:k<=4;k++){
+        
+      }
+    }
+  }
+  return 0;
 }
